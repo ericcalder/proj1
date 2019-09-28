3e683072af8369432bf599bf14387244b5f67a24
ericcalder <ericcalder@me.com>
2019-09-29 00:01:00 +0100

    with middleware hash and post2

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var connection = mysql.createConnection({
 
 var app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended : true}));
+//app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
