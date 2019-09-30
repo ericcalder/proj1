d20e5458d9d49ca512ff62ebd6405e642658230c
ericcalder <ericcalder@me.com>
2019-10-01 00:38:10 +0100

    with capital.gif

--- app.js
+++ app.js
@@ -112,6 +112,9 @@ const isLoggedIn=(req, res, next)=>{
 
 app.get('/', function(req,res){
 	console.log('in root');
+	if(req.headers.cookie){
+	console.log('req.header  :'+JSON.stringify(req.headers.cookie));
+	}
 res.render('login');
 });
 
