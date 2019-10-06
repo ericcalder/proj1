b99bc1033f2803e73bc0ee6fc8989d1c3984a9e4
ericcalder <ericcalder@me.com>
2019-10-07 00:20:23 +0100

    no index router

--- app.js
+++ app.js
@@ -138,7 +138,8 @@ app.get('/logout', function (req, res) {
 //		console.log('in logout '+JSON.stringify(req.cookies))
 //		res.clearCookie("user_sid","",{ expires: new Date() });
 //		console.log('cleared cookie:'+JSON.stringify(req.cookies))
-//		delete req.session.loggedin;
+		delete req.session.loggedin;
+		res.clearCookie("user_sid","")
 //		console.log('req.session===='+req.session.loggedin)
 		res.redirect('/');
 		
