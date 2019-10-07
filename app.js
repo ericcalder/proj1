97199848e9e8fdf0a888254719fb55e15a2c1949
ericcalder <ericcalder@me.com>
2019-10-07 01:11:45 +0100

    no index router

--- app.js
+++ app.js
@@ -67,8 +67,7 @@ app.use(session({
     //}
     cookie: {
         path: "/",
-        secure: true,
-        //domain: ".herokuapp.com", REMOVE THIS HELPED ME (I dont use a domain anymore)
+        //secure: true,
         httpOnly: true
     }
 }));
