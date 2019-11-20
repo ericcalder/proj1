0d54b084acd5b599947c3e507141c29c3ff2f9ec
ericcalder <ericcalder@me.com>
2019-11-20 23:24:33 +0000

    new1

--- app.js
+++ app.js
@@ -134,8 +134,8 @@ app.post('/', hashpw, logIn,  function(req, res){
 	console.log('in post  '+port)
 	console.log('req.session===='+req.session.loggedin)
 	if(req.session.loggedin){
-		//console.log('conn options='+JSON.stringify(options))
-		//res.render('index',{user:req.session.email})
+		console.log('conn options='+JSON.stringify(options))
+		res.render('index',{user:req.session.email})
 		res.redirect('/index')
 	}
 	else {res.send('not logged in')}
