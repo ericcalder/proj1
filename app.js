d79d2921b28ae7d965bb868ed6dd6638f67ce89c
ericcalder <ericcalder@me.com>
2019-11-22 23:59:13 +0000

    new

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ if (port == null || port == "") {
 }
 
 if(process.env.JAWSDB_URL){
-var connection =  mysql.createConnection(process.env.JAWSDB_URL);
-/*
-mysql.createConnection({
+//var connection =  mysql.createConnection(process.env.JAWSDB_URL);
+
+var connection=mysql.createConnection({
 	host	: process.env.JAWSDB_HOST,
 	user 	: process.env.JAWSDB_USER,
 	password: process.env.JAWSDB_PW,
-	database: process.env.JAWSDB_DB,
-	port    : process.env.JAWSDB_PORT   
+	database: process.env.JAWSDB_DB
+	  
 });
-*/  
+  
 }
 else {
 var connection = mysql.createConnection({
